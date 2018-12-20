fff6d437a58775c18be0585b21a8ea0a2701c887
Ravish Kumar <ravish5star@gmail.com>
2018-12-20 04:38:01 -0800

    before Authentication firebase

--- app/src/main/java/com/example/ravish/testro/ResturantDetail.java
+++ app/src/main/java/com/example/ravish/testro/ResturantDetail.java
@@ -1,8 +1,10 @@
 package com.example.ravish.testro;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResturantDetail extends AppCompatActivity {
     private ViewPager viewPager;
@@ -14,5 +16,10 @@ public class ResturantDetail extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         myadapter = new SlideAdapter(this);
         viewPager.setAdapter(myadapter);
+    }
+    public void back(View view){
+        Intent intent = new Intent(getBaseContext(),Resturants.class);
+        startActivity(intent);
+
     }
 }
